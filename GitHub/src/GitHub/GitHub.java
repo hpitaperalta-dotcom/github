85a83667cf012870b493eafebf66c1562b22872c
hpitaperalta-dotcom <hpitaperalta@insgabrielamistral.cat>
2026-03-05 10:20:59 +0100

    3a modificacio

--- GitHub/src/GitHub/GitHub.java
+++ GitHub/src/GitHub/GitHub.java
@@ -5,6 +5,7 @@ public class GitHub {
 	public static void main(String[] args) {
 		System.out.println("1a modificació Git");
 		System.out.println("2a modificació GitHub");
+		System.out.println("3a modificació Git");
 
 	}
 
